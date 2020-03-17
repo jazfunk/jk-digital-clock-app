b27a228fca07705bb5cd0ac4bbe1be22b58f0e6c
Jeff King <jazfunk@gmail.com>
2020-03-17 17:39:26 -0400

    Added time format switching button and code.  Isn't working properly yet

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function showCurrentTime(){
+
+// Run function first time
+window.onload = function() {
+    showCurrentTime(12);
+    // Add click event listener for te button
+    document.getElementById("btn24hr").addEventListener("click", changeTimeFormat);
+}
+
+function showCurrentTime(timeFormat){
 
     // Fill Array to convert Month (Integer) to Month (String)
     var monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -23,10 +31,14 @@ function showCurrentTime(){
     // Set hour variable
     var hour = today.getHours();
 
+    // If 12 hour time format is chosen
     // If 24-hour value is 0, then change value to 12
     // ElseIf 24-hour is greater than 12, then change value to 24-hour value - 12
     // Else retain 24-hour value
-    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+    // Else retain 24-hour value
+    if (timeFormat == 12) {
+        hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+    } 
     
     // Get current hour, minute, and second; adding a "0" before single digit numbers
     // If hour is less than 10, then affix a leading "0" to maintain 2-Digit values
@@ -36,7 +48,12 @@ function showCurrentTime(){
     var sec = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
 
     // Determine Am/Pm
-    var amOrPM = today.getHours() >= 12 ? "PM" : "AM";
+    if (timeFormat == 12) {
+        var amOrPM = today.getHours() >= 12 ? "PM" : "AM";
+    } else {
+        var amOrPM = "24";        
+    }
+    
 
     // Set values for div element ID's
     document.getElementById("dayOfWeekAndDate").innerText = 
@@ -51,13 +68,25 @@ function showCurrentTime(){
     document.getElementById("secondsDisplay").innerText = 
                 ": " + sec;
 
-
-                
     //var displayTime = setTimeout(function(){ showCurrentTime() }, 1000);
     //setTimeout(function(){ showCurrentTime() }, 1000);
-    setTimeout(showCurrentTime, 1000)
+    setTimeout(showCurrentTime, 1000, (timeFormat > 12) ? 24 : 12);
 }
 
-// Run function first time
-showCurrentTime();
+
+
+function changeTimeFormat(){
+    var tF = 0
+    // run showCurrentTime() with appropriate argument
+    // If time is currently PM, then we are in 12 hour mode, need to switch to 24 hour
+    if (document.getElementById("amPmDisplay").Value = "PM") {
+        tf = 24;
+    } else {
+        tf = 12;
+    }
+
+   showCurrentTime(tf);
+}
+
+
 
